d0541552713cf4c437587a91fa1af40d4d068560
UsuarioTemporal <thomromanaguilar@gmail.com>
2019-01-08 16:51:33 -0500

    JSON  y AJAX

---
+++ ajax/script_2.js
@@ -0,0 +1,13 @@
+document.getElementById('json').addEventListener('click',e=>{
+    const xhttp = new XMLHttpRequest()
+
+    xhttp.open('GET','catalogo.json',true)
+    xhttp.send()
+
+    xhttp.onreadystatechange = function (){
+        if(this.readyState===4 && this.status===200){
+            console.log(this.responseText)
+        }
+    }
+})
+console.log("sincronizado")
