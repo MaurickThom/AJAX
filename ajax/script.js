01dbcc1a3bde9ed54d3eeca83b300e4f39c94181
UsuarioTemporal <thomromanaguilar@gmail.com>
2019-01-08 14:26:14 -0500

    Peticiones al servidor

--- ajax/script.js
+++ ajax/script.js
@@ -1,3 +1,8 @@
+/**
+ * Las posibilidades de hacer peticiones al servidor sin tener que volver a cargar la página
+ */
+
+// peiition HTTP al servidor
 document.querySelector('#btn').addEventListener('click',e=>{
     console.log('Función activada')
     let xhttp = new XMLHttpRequest()
