3c8df91e1ff497baa2d728392c5cc4b5d4483f15
UsuarioTemporal <thomromanaguilar@gmail.com>
2019-01-08 18:15:54 -0500

    Imprimiendo Json mediante Ajax

--- ajax/script_2.js
+++ ajax/script_2.js
@@ -6,8 +6,17 @@ document.getElementById('json').addEventListener('click',e=>{
 
     xhttp.onreadystatechange = function (){
         if(this.readyState===4 && this.status===200){
-            console.log(JSON.parse(this.responseText))
+            let arr =JSON.parse(this.responseText).catalogo.slice()
+            
+            let element = document.getElementById('rest')
+            element.innerHTML=``
+            arr.map(e=>{
+                element.innerHTML+=`
+                <tr>
+                    <td>${e.titulo}</td>
+                    <td>${e.artista}</td>
+                </tr>`
+            })
         }
     }
 })
-console.log("sincronizado")
