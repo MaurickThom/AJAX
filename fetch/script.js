c262ab2f8387aa6564b714dc7cea1a3830c207b8
UsuarioTemporal <thomromanaguilar@gmail.com>
2019-01-10 03:53:46 -0500

    Fetch : mas simplificado que Ajax

--- fetch/script.js
+++ fetch/script.js
@@ -4,7 +4,13 @@ const btnEvent = container=>{
         if(element.tagName==='BUTTON'){
             fetch('archivo.txt').
             then(data=>data.text()). // transformando a un archivo text
-            then()
+            then(data=>{
+                console.log(data)
+                let elementoInterno = container.querySelector('#parrafo')
+                elementoInterno.innerHTML=``
+                elementoInterno.innerHTML=`<p>${data}</p>`
+                
+            })
 
         }
     })
