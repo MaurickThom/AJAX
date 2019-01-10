bd78013238b4e6b74b3b7f0b212644e089d9ada8
UsuarioTemporal <thomromanaguilar@gmail.com>
2019-01-10 17:16:45 -0500

    Usando localStorage

--- localstorage/script.js
+++
@@ -1,8 +0,0 @@
-const apiAlmacenamiento = ()=>{
-    if (typeof Storage !== undefined ){
-        alert('Tu navegador si soporta el api de Storage')
-    }else{
-        alert('Tu navegador no soporta')
-    }
-}
-apiAlmacenamiento()
