d80b7574ac20167610e8d76467ce3968dc170d33
UsuarioTemporal <thomromanaguilar@gmail.com>
2019-01-11 15:36:06 -0500

    Casi terminado CRUD

--- localstorage/script.js
+++ localstorage/script.js
@@ -13,7 +13,6 @@ const crearItem = (actividad)=>{
 }
 const guardarDB = ()=>{
     localStorage.setItem('items',JSON.stringify(arrayActividades))
-
 }
 
 const generarHTML = (container,actividad)=>{
@@ -25,6 +24,32 @@ const generarHTML = (container,actividad)=>{
         </div>
     `
     return container.innerHTML
+}
+
+const pintar = ()=>{
+    listaActividadUI.innerHTML=``
+    arrayActividades=JSON.parse(localStorage.getItem('items'))
+    if(arrayActividades===null){
+        arrayActividades=[]
+    }else{
+        arrayActividades.forEach(element => {
+            console.log(element)
+            listaActividadUI.innerHTML=generarHTML(listaActividadUI,element)
+        });
+    }
+}
+
+const eliminarDB = (actividad)=>{
+    let indexArray ;
+    arrayActividades.forEach((element,index) => {
+        if(actividad.firstElementChild.innerHTML.trim()===element.actividad){
+            
+            indexArray=index
+        }
+    });
+    arrayActividades.splice(indexArray,1)
+    guardarDB()
+    pintar()
 }
 const containerEvent = container=>{
     container.addEventListener('submit',e=>{
@@ -42,24 +67,18 @@ const containerEvent = container=>{
         let i = e.target
         if(i.id==='aprobar'){
             let l= e.path[1].firstElementChild.innerHTML.trim()
-            
+            console.log(l)
         }else if(i.id==='borrar'){
-
+            let l= e.path[1]
+            console.log(l)
+            eliminarDB(l)
         }
         
     })
 }
 document.addEventListener('DOMContentLoaded',e=>{
     listaActividadUI.innerHTML=``
-    arrayActividades=JSON.parse(localStorage.getItem('items'))
-    if(arrayActividades===null){
-        arrayActividades=[]
-    }else{
-        arrayActividades.forEach(element => {
-            console.log(element)
-            listaActividadUI.innerHTML=generarHTML(listaActividadUI,element)
-        });
-    }
+    pintar()
 
 })
 containerEvent(document.getElementById('container'))
