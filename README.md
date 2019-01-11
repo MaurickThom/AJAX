7e815dec2d66b782781c5cd50afd5e2fbcf7a0c3
UsuarioTemporal <thomromanaguilar@gmail.com>
2019-01-11 01:35:04 -0500

    Simplificando el código con la función generarHTML

--- README.md
+++ README.md
@@ -6,4 +6,4 @@
 - biblioteca  y/o libreria: Es un conjunto de elementos (funciones,clases,variables )
 - API : Interfaz para programacion de aplicaciones, es la parte de una biblioteca a la que accede un programa , esto te ayudará a interar con el programa sin tener acceso
 - framework : es un conjunto integrado de herramientas que facilitan un desarrollo de software,Puede incluir APIs y bibliotecas independiente
-- SDK : 
+- SDK : software developer kit , es similar a un framework con la diferencia de que se orienta al desarrollo de aplicaiones exclusivas para una plataforma
--- localstorage/script.js
+++ localstorage/script.js
@@ -15,6 +15,17 @@ const guardarDB = ()=>{
     localStorage.setItem('items',JSON.stringify(arrayActividades))
 
 }
+
+const generarHTML = (container,actividad)=>{
+    container.innerHTML+=`
+        <div>
+            Actividad : <b>${actividad.actividad}</b> , estado : ${actividad.estado}
+            <span id="aprobar">Aprobar</span> : 
+            <span id="borrar">Borrar</span>
+        </div>
+    `
+    return container.innerHTML
+}
 const containerEvent = container=>{
     container.addEventListener('submit',e=>{
         e.preventDefault()
@@ -23,13 +34,7 @@ const containerEvent = container=>{
             let actividad = crearItem(texto.value)
             guardarDB()
             formularioUI.reset()
-            listaActividadUI.innerHTML+=`
-                <div>
-                Actividad : <b>${actividad.actividad}</b> , estado : ${actividad.estado}
-                <span id="aprobar">Aprobar</span> : 
-                <span id="borrar">Borrar</span>
-                </div>
-            `
+            listaActividadUI.innerHTML=generarHTML(listaActividadUI,actividad)
         }
     })
     listaActividadUI.addEventListener('click',e=>{
@@ -52,13 +57,7 @@ document.addEventListener('DOMContentLoaded',e=>{
     }else{
         arrayActividades.forEach(element => {
             console.log(element)
-            listaActividadUI.innerHTML+=`
-                <div>
-                Actividad :  <b>${element.actividad}</b> , estado : ${element.estado}
-                <span id="aprobar">Aprobar</span> : 
-                <span id="borrar">Borrar</span>
-                </div>
-            `
+            listaActividadUI.innerHTML=generarHTML(listaActividadUI,element)
         });
     }
 
