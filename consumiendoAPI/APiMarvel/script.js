ffdff6eb7da90f7a12ebfe0f51b0f007667a1b28
UsuarioTemporal <thomromanaguilar@gmail.com>
2019-01-13 23:31:34 -0500

    Conexion a la API

--- consumiendoAPI/APiMarvel/script.js
+++ consumiendoAPI/APiMarvel/script.js
@@ -1,1 +1,10 @@
 // alert('Hola marvel')
+const privateKey = '06eb46803dff88ff074014850f23bc0bdb55fca4',
+    publicKey = 'af22973d494d692ad4119c57f8694119',
+    content=document.getElementById('content')
+
+// Método para la conexión
+
+const getConnection = ()=>{
+    
+}
