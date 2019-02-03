7860667100088fc77cb90b59aad42fe4aee280a4
UsuarioTemporal <thomtwd@gmail.com>
2019-02-03 18:32:34 -0500

    Comenazando con la lógica ,se usará generadores y programación asíncrona

--- consumiendoAPI/3/scritps.js
+++ consumiendoAPI/3/scritps.js
@@ -3,7 +3,13 @@ class DataRetriever{ // recolector de datos
         console.log('Instanciado correctamente')
     }
     static get TRIGGER_ELM(){
-        return document.querySelector()
+        return document.querySelector('.user-button')
+    }
+    static get INPUT_ELM(){
+        return document.querySelector('.user-input')
+    }
+    static get BASE_URL(){
+        return `https://jsonplaceholder.typicode.com`
     }
 }
 let dataRetriever  = new DataRetriever();
