b17372be8b2e74e36b3049558cec12736b290e02
UsuarioTemporal <thomtwd@gmail.com>
2019-02-04 01:25:54 -0500

    Usando Ajax

--- consumiendoAPI/4/script.js
+++ consumiendoAPI/4/script.js
@@ -1,5 +1,6 @@
 // let d= md5('Devnami Youtube Channel');
 // console.log(d);
+// estas llaves no son mias :v , lo copie de otro men xdxdxd
 const privateKey = '7c55664c217553d96ed6e84098a931dff0204776',
     publicKey = 'a71fa6c845e117349196fe9c4a58b2ae',
     content = document.getElementById('content')
@@ -9,6 +10,8 @@ const getConnection = ()=>{
         hash = md5(ts+privateKey+publicKey),
         URL = `http://gateway.marvel.com/v1/public/characters?ts=${ts}&apikey=${publicKey}&hash=${hash}`
     
-    console.log(URL)
+    fetch(URL)
+    .then(response=>response.json())
+    .then(data=>console.log(data))
 }
 getConnection()
