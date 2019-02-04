3825d69c1a672e47264a4291ab758eeaa59870b3
UsuarioTemporal <thomtwd@gmail.com>
2019-02-04 01:47:04 -0500

    Api de marvel

--- consumiendoAPI/4/script.js
+++ consumiendoAPI/4/script.js
@@ -1,10 +1,17 @@
 // let d= md5('Devnami Youtube Channel');
 // console.log(d);
-// estas llaves no son mias :v , lo copie de otro men xdxdxd
+// estas llaves no son mias :v , las copie de otro men xdxdxd
 const privateKey = '7c55664c217553d96ed6e84098a931dff0204776',
     publicKey = 'a71fa6c845e117349196fe9c4a58b2ae',
     content = document.getElementById('content')
-
+const drawHero = hero=>{
+    content.innerHTML=`
+        <div>
+            <h3>${hero.name}</h3>
+            <img src="${hero.thumbnail.path}/portrait_uncanny.${hero.thumbnail.extension}">
+        </div>
+    `
+}
 const getConnection = ()=>{
     const ts = Date.now(),
         hash = md5(ts+privateKey+publicKey),
@@ -12,6 +19,10 @@ const getConnection = ()=>{
     
     fetch(URL)
     .then(response=>response.json())
-    .then(data=>console.log(data))
+    .then(response=>{
+        response.data.results.forEach(element => {
+            drawHero(element)
+        });
+    })
 }
 getConnection()
