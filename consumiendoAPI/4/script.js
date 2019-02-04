f483cbd31a3090f56030ca657b57300fcc7297e4
UsuarioTemporal <thomtwd@gmail.com>
2019-02-04 02:28:32 -0500

    Lógica terminada

--- consumiendoAPI/4/script.js
+++ consumiendoAPI/4/script.js
@@ -3,7 +3,23 @@
 // estas llaves no son mias :v , las copie de otro men xdxdxd
 const privateKey = '7c55664c217553d96ed6e84098a931dff0204776',
     publicKey = 'a71fa6c845e117349196fe9c4a58b2ae',
-    content = document.getElementById('content')
+    content = document.getElementById('content'),
+    search = document.getElementById('search')
+
+const searchHero = name=>{
+    const ts = Date.now(),
+        hash = md5(ts+privateKey+publicKey),
+        hero = encodeURIComponent(name),
+        URL = `http://gateway.marvel.com/v1/public/characters?name=${hero}&ts=${ts}&apikey=${publicKey}&hash=${hash}`
+    
+    fetch(URL)
+    .then(response=>response.json())
+    .then(response=>{
+        response.data.results.forEach(element => {
+            drawHero(element)
+        });
+    })
+}
 const drawHero = hero=>{
     content.innerHTML+=`
         <div>
@@ -12,6 +28,7 @@ const drawHero = hero=>{
             <p>${hero.description}</p>
         </div>
     `
+    // content.insertAdjacentHTML('beforeend',asdasd)
 }
 const getConnection = ()=>{
     const ts = Date.now(),
@@ -26,4 +43,12 @@ const getConnection = ()=>{
         });
     })
 }
+search.addEventListener('keyup',e=>{
+    
+    if(e.keyCode===13  ){
+        searchHero(e.target.value.trim())
+
+    }
+    
+})
 getConnection()
