bacb67b6d1b59b17629f3d1fe243f80c0586f827
UsuarioTemporal <thomtwd@gmail.com>
2019-02-04 01:49:54 -0500

    Cambiando variables

--- consumiendoAPI/4/script.js
+++ consumiendoAPI/4/script.js
@@ -5,10 +5,11 @@ const privateKey = '7c55664c217553d96ed6e84098a931dff0204776',
     publicKey = 'a71fa6c845e117349196fe9c4a58b2ae',
     content = document.getElementById('content')
 const drawHero = hero=>{
-    content.innerHTML=`
+    content.innerHTML+=`
         <div>
             <h3>${hero.name}</h3>
             <img src="${hero.thumbnail.path}/portrait_uncanny.${hero.thumbnail.extension}">
+            <p>${hero.description}</p>
         </div>
     `
 }
