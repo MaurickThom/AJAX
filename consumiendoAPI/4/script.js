5094fab1f6bc013061cbfae81faac7465c03fce5
UsuarioTemporal <thomtwd@gmail.com>
2019-02-04 01:23:15 -0500

    APi marvel

--- consumiendoAPI/4/script.js
+++ consumiendoAPI/4/script.js
@@ -1,2 +1,14 @@
-let d= md5('Devnami Youtube Channel');
-console.log(d);
+// let d= md5('Devnami Youtube Channel');
+// console.log(d);
+const privateKey = '7c55664c217553d96ed6e84098a931dff0204776',
+    publicKey = 'a71fa6c845e117349196fe9c4a58b2ae',
+    content = document.getElementById('content')
+
+const getConnection = ()=>{
+    const ts = Date.now(),
+        hash = md5(ts+privateKey+publicKey),
+        URL = `http://gateway.marvel.com/v1/public/characters?ts=${ts}&apikey=${publicKey}&hash=${hash}`
+    
+    console.log(URL)
+}
+getConnection()
