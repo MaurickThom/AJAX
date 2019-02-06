51f9fff8aa76203dab122a888e3176b8f6530b1a
UsuarioTemporal <thomtwd@gmail.com>
2019-02-06 05:17:28 -0500

    Ultimo commit y a dormir

--- consumiendoAPI/5/script.js
+++ consumiendoAPI/5/script.js
@@ -1,2 +1,9 @@
 const form = document.getElementById('form')
 const button = document.getElementById('button')
+const filtrar = ()=>{
+    console.log(form.value)
+}
+button.addEventListener('click',e=>{
+    e.preventDefault()
+    filtrar()
+})
