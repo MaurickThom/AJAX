ccca423e5ceea4dde136c2e361bea80a4a166f2b
UsuarioTemporal <thomtwd@gmail.com>
2019-02-06 12:44:32 -0500

    Terminado

--- consumiendoAPI/5/script.js
+++ consumiendoAPI/5/script.js
@@ -1,9 +1,43 @@
 const form = document.getElementById('form')
 const button = document.getElementById('button')
+const items = document.getElementById('items')
+const productos=[
+    {
+        nombre:'Platanos',
+        valor : 500
+    },
+    {
+        nombre:'Pera',
+        valor : 500 
+    },
+    {
+        nombre:'Sandia',
+        valor : 500
+    },
+    {
+        nombre:'Melon',
+        valor : 500 
+    }
+]
 const filtrar = ()=>{
-    console.log(form.value)
+    // console.log(form.value)
+    const text = form.value.toLowerCase()
+    items.innerHTML=``
+    productos.forEach(producto=>{
+        let nombre = producto.nombre.toLowerCase()
+        // console.log(nombre)
+        if(nombre.indexOf(text)!==-1){
+            items.innerHTML+=`<li>${producto.nombre}-${producto.valor}</li>`
+        }
+        
+    })
+    if(items.innerHTML===''){
+        items.innerHTML=`<p>Elemento no encontrado</p>`
+    }
 }
 button.addEventListener('click',e=>{
     e.preventDefault()
     filtrar()
 })
+form.addEventListener('keyup',filtrar)
+filtrar()
